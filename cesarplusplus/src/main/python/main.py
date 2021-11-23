f8419e73622d3171ce951749bd4814c7abc9acdd
Lucas Mendes <lefm@cesar.school>
2021-11-23 20:04:12 -0300

    Ajustes script de correcao para formatar e nao considerar linebreaks

--- cesarplusplus/src/main/python/main.py
+++ cesarplusplus/src/main/python/main.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 import os
 import sys
 import ast
@@ -16,34 +17,36 @@ if __name__ == "__main__":
 		output = ast.literal_eval(f.read())
 	
 
-	report = open('report.txt','w')
-	report.close()
-	report = open('report.txt','a')
+	report = open('report.txt','w', encoding='UTF-8')
 
 	tests = [0] * len(input)
 
-	os.system("gcc /../questions/" + sub_path + ".c -o submission")
+	os.system("gcc -std=c99 ../questions/" + sub_path + ".c -o submission")
 	
-	if not os.path.isfile("/../questions/submission"):
+	if not os.path.isfile("submission.exe"):
 		report.write(sub_path + ".c fail on compilation\n")
 
-	if os.path.isfile("submission"):
+	if os.path.isfile("submission.exe"):
 		for index, i in enumerate(input):
 
-			process = subprocess.Popen('./submission', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+			process = subprocess.Popen('./submission.exe', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 			process.stdin.write(i.encode('UTF-8'))
 			process.stdin.close()
 			usr_out = process.stdout.read().decode('UTF-8')
+			usr_out = re.sub(r'\r\n', r'\n', usr_out)
+
 			process.wait()
 
-			if not (usr_out == output[index]):
-				report.write(str(index + 1) + " Expected: " + output[index] + "\n\nnot\n" + usr_out + "\n\n")
+			if not (usr_out.split() == output[index].split()):
+				if (usr_out[0] != '\n'):
+					usr_out = '\n' + usr_out
+				report.write("Caso " + str(index + 1) + " X\n\nExpected: " + re.sub(r'\n', r'\n\t', output[index]) + "\nNot:" + re.sub(r'\n', r'\n\t', usr_out) + "\n\n")
 			else:
 				tests[index] = 1
-				report.write(str(index + 1) + " Correct!!\n")
+				report.write("Caso " + str(index + 1) + " ✓\n")
 
-		
 		report.write("\ntests: " + " | ".join(str(e) for e in tests))
+		report.write("\nTotal: " + str(sum(tests)) + "/" + str(len(tests)))
 
 	report.close()
 
--- cesarplusplus/src/main/questions/q1_lefm.c
+++ cesarplusplus/src/main/questions/q1_lefm.c
@@ -11,7 +11,7 @@ int main(){
 
     for (int i = 0; i < eleitores; i++){
         for (int j = 0; j < princesas; j++){
-            scanf("%d", &votacao[i][j]);
+            scanf("%d", &votacao[j][i]);
         }
         
     }
